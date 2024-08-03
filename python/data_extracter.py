779136be3249722811bc9085ce993c37413c97d2
ltang2 <letiant@andrew.cmu.edu>
2024-08-02 22:53:04 -0400

    working towards data conversion

--- python/data_extracter.py
+++ python/data_extracter.py
@@ -1,9 +1,26 @@
 from glob import glob
 from coffea import util
 import awkward as ak
+import torch
+import numpy as np
+import uproot
 
 sample = 'fourTag'
 custom_selection = 'event.preselection'  # region on which you want to train
+
+train_valid_modulus = 3
+def coffea_to_tensor(event, device = 'cpu', decode = False, kfold=False):
+    j = torch.FloatTensor( event['Jet',('pt','eta','phi','mass')].to_numpy().view(np.float32).reshape(-1, 4, 4) ) # [event,jet,feature]
+    j = j.transpose(1,2).contiguous() # [event,feature,jet]
+    e = torch.LongTensor( np.asarray(event['event'], dtype=np.uint8) )%train_valid_modulus
+    if kfold:
+        return j, e
+    w = torch.FloatTensor( event['weight'].to_numpy().view(np.float32) )
+    R  = 1*torch.LongTensor( event['SB'].to_numpy().view(np.uint8) )
+    R += 2*torch.LongTensor( event['SR'].to_numpy().view(np.uint8) )
+    if device != 'cpu':
+        j, w, R, e = j.to(device), w.to(device), R.to(device), e.to(device)
+    return j, w, R, e
 
 def load(cfiles, selection=''):
     event_list = []
@@ -15,12 +32,18 @@ def load(cfiles, selection=''):
         event_list.append(event)
     return ak.concatenate(event_list)
 
+def torch2ak(x):
+    return ak.Array(ak.Array(x.cpu().detach().numpy()))
+
 coffea_file = sorted(glob(f'data/{sample}_picoAOD*.coffea')) # file used for autoencoding
 
 # Load data
 event = load(coffea_file, selection=custom_selection)
+j, w, R, e = coffea_to_tensor(event, device='cpu')
 
-print(type(event))
-print(event.fields)
-print(event.layout)
-print(event)
+# store data
+file = uproot.recreate(f'data/toy_data.root')
+
+
+
+
