9e810883f268007882b3ce74fe31e29ffc025917
ltang2 <letiant@andrew.cmu.edu>
2024-08-05 21:34:27 -0400

    data extraction and augmentation complete, fix issues

--- python/data_extractor.py
+++ python/data_extractor.py
@@ -4,6 +4,11 @@ import awkward as ak
 import torch
 import numpy as np
 import uproot
+from networks import utils
+
+num_rotations = 50
+seed = 88
+torch.manual_seed(seed)
 
 sample = 'fourTag'
 custom_selection = 'event.preselection'  # region on which you want to train
@@ -36,19 +41,27 @@ def torch2ak(x):
     return ak.Array(ak.Array(x.cpu().detach().numpy()))
 
 def jet2dict(jet):
-    return {'pt': torch2ak(jet[:,:,0]), 'eta': torch2ak(jet[:,:,1]), 'phi': torch2ak(jet[:,:,2]), 'mass': torch2ak(jet[:,:,3])}
+    return {'pt': torch2ak(jet[:,0,:]), 'eta': torch2ak(jet[:,1,:]), 'phi': torch2ak(jet[:,2,:]), 'mass': torch2ak(jet[:,3,:])}
 
 coffea_file = sorted(glob(f'data/{sample}_picoAOD*.coffea')) # file used for autoencoding
 
 # Load data
 event = load(coffea_file, selection=custom_selection)
 j, w, R, e = coffea_to_tensor(event, device='cpu')
+print(j.shape)
 
 # store data
-file = uproot.recreate(f'data/toy_data.root')
-file['jet'] = jet2dict(j)
+with uproot.recreate(f'data/toy_data.root') as file:
+    file['jet'] = jet2dict(j)
 
-# symmetrize the data
-
-
-
+# data augmentation
+eta_invert = j.clone()
+eta_invert[:,1,:] *= -1
+eta_symmetric = torch.cat([j, eta_invert], dim=0)
+phi_symmetric = eta_symmetric.repeat(num_rotations, 1, 1)
+num_events = phi_symmetric.shape[0]
+phi_rotations = -torch.pi + torch.rand(num_events, 1) * torch.pi
+phi_symmetric[:,2,:] += phi_rotations
+filepath = 'data/toy_data_augmented_seed{}_phirot{}.root'.format(seed, num_rotations)
+with uproot.recreate(filepath) as file:
+    file['jet'] = jet2dict(phi_symmetric)
