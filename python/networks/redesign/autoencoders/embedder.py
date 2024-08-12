ff3813db996e1e3318108401eeaf109f67b46dd3
ltang2 <letiant@andrew.cmu.edu>
2024-08-12 19:52:11 -0400

    working on assembling the full model

--- python/networks/redesign/autoencoders/embedder.py
+++ python/networks/redesign/autoencoders/embedder.py
@@ -1,14 +1,6 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from .base import BaseAutoencoder
 
-class EmbedderAE(nn.Module):
+class EmbedderAE(BaseAutoencoder):
     def __init__(self, input_dim, hidden_dim, output_dim):
-        super(EmbedderAE, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        pass
